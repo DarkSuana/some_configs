faf7bdead409c26f44c9769e929db28dc8a871f8
DarkSuana <53618389+DarkSuana@users.noreply.github.com>
2022-02-06 14:03:07 +0300

    Update isc-dhcp-server.sh

--- isc-dhcp-server.sh
+++ isc-dhcp-server.sh
@@ -1,7 +1,6 @@
 #!bin/bash
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install isc-dhcp-server git -y
-git clone https://github.com/DarkSuana/some_configs.git
 cd some_configs
 rm -Rf /etc/dhcp/dhcpd.conf
 cp dhcpd.conf /etc/dhcp/dhcpd.con
