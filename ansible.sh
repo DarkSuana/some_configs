a2f34b432439ef72b0e1f6942dc9eb1f4bd40726
DarkSuana <53618389+DarkSuana@users.noreply.github.com>
2022-02-06 14:41:30 +0300

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -1,5 +1,5 @@
 apt-get update && apt-get upgrade -y
-apt-get install python3 python3-pip openssh-server sshpass
+apt-get install python3 python3-pip openssh-server sshpass -y
 pip3 install ansible
 git clone https://github.com/DarkSuana/Ansible
 cd ansible
