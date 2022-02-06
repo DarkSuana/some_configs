b805ba83818b1aa6535813d4ac6d73e2e6ccec77
DarkSuana <53618389+DarkSuana@users.noreply.github.com>
2022-02-06 13:51:54 +0300

    Update debian_install_docker.sh

--- debian_install_docker.sh
+++ debian_install_docker.sh
@@ -4,5 +4,6 @@ sudo apt-get install gpg apt-transport-https ca-certificates curl software-prope
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list  /dev/null
 sudo apt-get update 
+sudo apt-get install docker-ce
 apt-cache policy docker-ce
 sudo systemctl status docker
