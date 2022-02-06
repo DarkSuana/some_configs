3f355831d35a0bfd2cd60a71e9e71ca84d5b6231
DarkSuana <53618389+DarkSuana@users.noreply.github.com>
2022-02-06 14:38:56 +0300

    Create ansible.sh

---
+++ ansible.sh
@@ -0,0 +1,6 @@
+apt-get update && apt-get upgrade -y
+apt-get install python3 python3-pip openssh-server sshpass
+pip3 install ansible
+git clone https://github.com/DarkSuana/Ansible
+cd ansible
+touch ansible.log
