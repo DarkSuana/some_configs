135da38a15f60bb40b94a719030cf8bcbf5a7c10
DarkSuana <Isstudents078@gmail.com>
2022-09-27 11:34:03 +0300

    рп

--- goflow+clickhouse.sh
+++ goflow+clickhouse.sh
@@ -4,6 +4,6 @@ apt-get install wget curl -y
 curl -fsSL https://get.docker.com | bash
 git clone https://github.com/h4sh5/goflow-clickhouse
 docker pull clickhouse/clickhouse-server
-docker run -d --name clickhouse-server --unlimit nofile=262144:262144 clickhouse/clickhouse-server
+docker run -d --name clickhouse-server --ulimit nofile=262144:262144 clickhouse/clickhouse-server
 cd goflow-clickhouse
 docker run --net=host -ti cloudflare/goflow:latests -kafka=false
