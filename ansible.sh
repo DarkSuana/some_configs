eab3b244e8b6cb4226b28c9fb7da803ee22ef42f
DarkSuana <53618389+DarkSuana@users.noreply.github.com>
2022-11-14 15:09:11 +0300

    Delete ansible.sh

--- ansible.sh
+++
@@ -1,6 +0,0 @@
-apt-get update && apt-get upgrade -y
-apt-get install python3 python3-pip openssh-server sshpass -y
-pip3 install ansible
-git clone https://github.com/DarkSuana/ansible_files
-cd ansible_files
-touch ansible.log
