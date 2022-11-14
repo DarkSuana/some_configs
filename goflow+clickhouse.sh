1e9828f9af7ed5bc416b8782960750520ab9ba8a
DarkSuana <53618389+DarkSuana@users.noreply.github.com>
2022-11-14 15:08:41 +0300

    Delete goflow+clickhouse.sh

--- goflow+clickhouse.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-apt-get update && apt-get upgrade -y
-apt-get install wget curl -y
-curl -fsSL https://get.docker.com | bash
-git clone https://github.com/h4sh5/goflow-clickhouse
-docker pull clickhouse/clickhouse-server
-docker run -d --name clickhouse-server --ulimit nofile=262144:262144 clickhouse/clickhouse-server
-cd goflow-clickhouse
-docker run --net=host -ti cloudflare/goflow:latests -kafka=false
